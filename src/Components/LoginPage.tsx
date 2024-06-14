ab93ecc7fa74cf7b769ddb56f79b61ebe2d53006
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2024-06-14 10:37:27 +0200

    update register and login
    add username and profile picture
    update page for error handling
    redirect user to login if register was successful
    update login page (not finished)

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -1,22 +1,108 @@
-import React, {useEffect} from 'react';
-import '../css/LoginPage.css';
-import logo from '../assets/logo_light.png'
-import discordLogo from '../assets/discord.svg'
+import React, {useEffect, useState} from 'react';
+import '../css/LoginPage.css'
+import config from "../config";
+import Logo from '../assets/logo.png'
+import {useNavigate} from "react-router-dom";
 
 function LoginPage() {
+    const navigation = useNavigate()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const emailRegex = RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-z0-9.-]+\\.[a-zA-Z]{2,}$')
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+
+        if(!emailRegex.test(email)) {
+            setErrorMessage('Invalid E-mail!')
+            return;
+        }
+
+        try {
+            console.log('Login with email:', email, 'and password:', password);
+            const response = await fetch(config.backendUrl + '/user/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email, password})
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Login successful', data);
+                return;
+            }
+
+            const r = await response.json()
+            setErrorMessage(r.error)
+            console.error('something went wrong: ', response)
+        } catch (error) {
+            console.error('registering failed', error);
+            // Handle error cases
+        }
+    };
+
+    useEffect(() => {
+        setErrorMessage('')
+    }, [email, password]);
+
     return (
-        <div className="bg-dark vh-100 vw-100 text-center bg-gradient">
-            <img className={'h-50'} src={logo} alt={'logo'}/>
-
-            <h2 className={'text-white mb-4'}>Login with</h2>
-
-            <a className={'btn btn-primary rounded-3'} style={{width: 256}} href={'http://localhost:3001/auth'}>
-                <img
-                    className={'card-img'}
-                    src={discordLogo}
-                    alt={'discord logo'}
-                />
-            </a>
+        <div className="container vh-100 align-content-center">
+            <div className="row justify-content-center">
+                <div className="col-xl-10 col-lg-12 col-md-9">
+                    <div className="card o-hidden border-0 shadow-lg my-5">
+                        <div className="card-body p-0">
+                            <div className="row">
+                                <div className="col-lg-6 d-none d-lg-block bg-login-image align-content-center">
+                                    <img src={Logo} alt={'Logo'} className={'img-fluid'}/>
+                                </div>
+                                <div className="col-lg-6 p-5">
+                                    <div className="text-center">
+                                        <h1 className="h4 text-gray-900 mb-4">Welcome back!</h1>
+                                    </div>
+                                    <form className="user" onSubmit={handleSubmit}>
+                                        <div className="form-group">
+                                            <input
+                                                type="email"
+                                                className="form-control form-control-user"
+                                                aria-describedby="emailHelp"
+                                                placeholder="Email address"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="form-group mb-2">
+                                            <input
+                                                type="password"
+                                                className="form-control form-control-user"
+                                                placeholder="Password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                required
+                                            />
+                                        </div>
+                                        <h3 className={'invalid-feedback d-block text-center'}
+                                            style={{minHeight: 16}}>{errorMessage}</h3>
+                                        <button type="submit" className="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                    </form>
+                                    <hr/>
+                                    <div className="text-center">
+                                        <a className="small" onClick={() => navigation('/register')}>
+                                            Create new account.
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Components/RegisterPage.tsx
+++ src/Components/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/LoginPage.css'
 import config from "../config";
 import Logo from '../assets/logo.png'
@@ -6,12 +6,31 @@ import {useNavigate} from "react-router-dom";
 
 function RegisterPage() {
     const navigation = useNavigate()
+    const [username, setUsername] = useState('');
+    const [picture, setPicture] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
-    const handleLogin = async (e: any) => {
+    const emailRegex = RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-z0-9.-]+\\.[a-zA-Z]{2,}$')
+
+    const handleSubmit = async (e: any) => {
         e.preventDefault()
+
+        if(!emailRegex.test(email)) {
+            setErrorMessage('Invalid E-mail!')
+            return;
+        }
+
+        let usernameToInsert = username;
+        if(username.length <= 0) {
+            usernameToInsert = email.substring(0, email.indexOf('@'));
+        }
+
+        let pictureToInsert = picture;
+        if(picture.length <= 0) {
+            pictureToInsert = 'https://ui-avatars.com/api/?name=' + usernameToInsert;
+        }
 
         try {
             console.log('registering with email:', email, 'and password:', password);
@@ -20,21 +39,29 @@ function RegisterPage() {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({email, password})
+                body: JSON.stringify({email, password, username: usernameToInsert, picture: pictureToInsert})
             });
 
             if (response.ok) {
                 const data = await response.json();
                 console.log('Register successful', data);
+                navigation('/login')
                 return;
             }
 
+            const r = await response.json()
+            setErrorMessage(r.error)
             console.error('something went wrong: ', response)
         } catch (error) {
             console.error('registering failed', error);
             // Handle error cases
         }
     };
+
+    useEffect(() => {
+        setErrorMessage('')
+    }, [email, password]);
+
     return (
         <div className="container vh-100 align-content-center">
             <div className="row justify-content-center">
@@ -45,43 +72,62 @@ function RegisterPage() {
                                 <div className="col-lg-6 d-none d-lg-block bg-login-image align-content-center">
                                     <img src={Logo} alt={'Logo'} className={'img-fluid'}/>
                                 </div>
-                                <div className="col-lg-6">
-                                    <div className="p-5">
+                                <div className="col-lg-6 p-5">
                                         <div className="text-center">
                                             <h1 className="h4 text-gray-900 mb-4">Create an free account!</h1>
                                         </div>
-                                        <form className="user" onSubmit={handleLogin}>
-                                            <div className="form-group">
-                                                <input
-                                                    type="email"
-                                                    className="form-control form-control-user"
-                                                    aria-describedby="emailHelp"
-                                                    placeholder="Username"
-                                                    value={email}
-                                                    onChange={(e) => setEmail(e.target.value)}
-                                                    required
-                                                />
-                                            </div>
-                                            <div className="form-group">
-                                                <input
-                                                    type="password"
-                                                    className="form-control form-control-user"
-                                                    placeholder="Password"
-                                                    value={password}
-                                                    onChange={(e) => setPassword(e.target.value)}
-                                                    required
-                                                />
-                                                <h3 className={'invalid-feedback d-block'}>{errorMessage}</h3>
-                                            </div>
-                                            <button type="submit" className="btn btn-primary btn-user btn-block">
-                                                Register
-                                            </button>
-                                        </form>
-                                        <hr/>
-                                        <div className="text-center">
-                                            <a className="small" onClick={() => navigation('/login')}>Log in existing
-                                                account.</a>
+                                    <form className="user" onSubmit={handleSubmit}>
+                                        <div className="form-group">
+                                            <input
+                                                type="email"
+                                                className="form-control form-control-user"
+                                                aria-describedby="emailHelp"
+                                                placeholder="Email address*"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                required
+                                            />
                                         </div>
+                                        <div className="form-group mb-2">
+                                            <input
+                                                type="password"
+                                                className="form-control form-control-user"
+                                                placeholder="Password*"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                required
+                                            />
+                                        </div>
+                                        <hr/>
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                className="form-control form-control-user"
+                                                placeholder="Username"
+                                                value={username}
+                                                onChange={(e) => setUsername(e.target.value)}
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                className="form-control form-control-user"
+                                                placeholder="Profile picture as link"
+                                                value={picture}
+                                                onChange={(e) => setPicture(e.target.value)}
+                                            />
+                                        </div>
+                                        <h3 className={'invalid-feedback d-block text-center'}
+                                            style={{minHeight: 16}}>{errorMessage}</h3>
+                                        <button type="submit" className="btn btn-primary btn-user btn-block">
+                                            Register
+                                        </button>
+                                    </form>
+                                    <hr/>
+                                    <div className="text-center">
+                                        <a className="small" onClick={() => navigation('/login')}>
+                                            Log in existing account.
+                                        </a>
                                     </div>
                                 </div>
                             </div>
